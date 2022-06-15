5d9e727d2631f3c0e70e9548ec1feb39d30f9bdd
Daphne Go <77186592+daphnejanelyn@users.noreply.github.com>
2022-06-15 12:15:43 +0800

    Update GO_D_WONG_K_managetools.c

--- GO_D_WONG_K_managetools.c
+++ GO_D_WONG_K_managetools.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-#include "translate.h"
+#include "GO_D_WONG_K_header.h"
 
 /*  getInput gets a string input from the user and 
     can get input with spaces between strings
